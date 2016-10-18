1c2dfbf71402381bbd9b0e23ef3835c043cb0d5a
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-10-18 15:13:36 +0300

    Initial Xcode project structure with .gitignore

---
+++ ZeroMQ/ZeroMQ.h
@@ -0,0 +1,19 @@
+//
+//  ZeroMQ.h
+//  ZeroMQ
+//
+//  Created by Ahmad M. Zawawi (azawawi) on 10/18/16.
+//  Copyright © 2016 azawawi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZeroMQ.
+FOUNDATION_EXPORT double ZeroMQVersionNumber;
+
+//! Project version string for ZeroMQ.
+FOUNDATION_EXPORT const unsigned char ZeroMQVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZeroMQ/PublicHeader.h>
+
+
