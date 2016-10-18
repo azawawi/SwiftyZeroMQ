5f554634b4fcb9a010dd5ef662ce01fcad8a994d
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-10-18 14:27:19 +0300

    It is compilable now

--- ZeroMQ/ZeroMQ.h
+++ ZeroMQ/ZeroMQ.h
@@ -1,10 +1,9 @@
-//
-//  ZeroMQ.h
-//  ZeroMQ
-//
-//  Created by Ahmad M. Zawawi (azawawi) on 10/18/16.
-//  Copyright © 2016 azawawi. All rights reserved.
-//
+/*
+Copyright (c) 2016 Ahmad M. Zawawi (azawawi)
+
+This package is distributed under the terms of the MIT license.
+Please see the accompanying LICENSE file for the full text of the license.
+*/
 
 #import <UIKit/UIKit.h>
 
