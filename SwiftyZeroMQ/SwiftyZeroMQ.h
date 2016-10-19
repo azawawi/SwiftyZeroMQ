b83adad0e03549274d6612c7a77e02673e761fa2
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-10-19 21:26:20 +0300

    Rename ZeroMQ.* -> SwiftyZeroMQ.*

---
+++ SwiftyZeroMQ/SwiftyZeroMQ.h
@@ -0,0 +1,17 @@
+//
+// Copyright (c) 2016 Ahmad M. Zawawi (azawawi)
+//
+// This package is distributed under the terms of the MIT license.
+// Please see the accompanying LICENSE file for the full text of the license.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyZeroMQ.
+FOUNDATION_EXPORT double SwiftyZeroMQVersionNumber;
+
+//! Project version string for SwiftyZeroMQ.
+FOUNDATION_EXPORT const unsigned char SwiftyZeroMQVersionString[];
+
+// In this header, you should import all the public headers of your framework
+// using statements like #import <SwiftyZeroMQ/PublicHeader.h>
