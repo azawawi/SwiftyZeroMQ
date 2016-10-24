f185915aa408c487b848939db19a11bcf1a5290e
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-10-24 15:40:53 +0300

    Test on more iOS versions in shell script

--- run-tests.sh
+++ run-tests.sh
@@ -4,18 +4,24 @@ command_exists () {
   type "$1" &> /dev/null
 }
 
+run_tests() {
+  echo "Running 'xcodebuild test' for $1. Please wait..."
+
+  xcodebuild \
+    -project SwiftyZeroMQ.xcodeproj \
+    -scheme SwiftyZeroMQ \
+    -sdk iphonesimulator \
+    -destination "$1" \
+    -verbose test | xcpretty
+}
+
 # Check whether xcpretty is installed
 if ! command_exists xcpretty; then
   echo "xcpretty not found. Please run 'gem install xcpretty'."
   exit 1
 fi
 
-DESTINATION='platform=iOS Simulator,name=iPhone 6,OS=10.0'
-
-echo "Running 'xcodebuild test' for $DESTINATION. Please wait..."
-xcodebuild \
-  -project SwiftyZeroMQ.xcodeproj \
-  -scheme SwiftyZeroMQ \
-  -sdk iphonesimulator \
-  -destination "$DESTINATION" \
-  -verbose test | xcpretty
+# Test on iPhone 5 (multiple iOS versions)
+run_tests 'platform=iOS Simulator,name=iPhone 5,OS=8.1'
+run_tests 'platform=iOS Simulator,name=iPhone 5,OS=9.0'
+run_tests 'platform=iOS Simulator,name=iPhone 5,OS=10.0'
