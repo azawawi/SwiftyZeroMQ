8ed854e0a9f83e5d09f4bc7312d8ed89bb330a44
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-10-26 10:34:10 +0300

    Less unneeded comments

--- SwiftyZeroMQ/SwiftyZeroMQ.h
+++ SwiftyZeroMQ/SwiftyZeroMQ.h
@@ -15,6 +15,3 @@ FOUNDATION_EXPORT double SwiftyZeroMQVersionNumber;
 
 //! Project version string for SwiftySwiftyZeroMQ.
 FOUNDATION_EXPORT const unsigned char SwiftyZeroMQVersionString[];
-
-// In this header, you should import all the public headers of your framework
-// using statements like #import <SwiftyZeroMQ/PublicHeader.h>
