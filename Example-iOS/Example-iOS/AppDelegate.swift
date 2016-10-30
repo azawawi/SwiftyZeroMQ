945e13f75d302c74a6e2aa46959497d5f0b462e3
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-10-30 10:40:50 +0200

    Add SwiftyZeroMQ framework and add a basic example that prints the library/framework version

--- Example-iOS/Example-iOS/AppDelegate.swift
+++ Example-iOS/Example-iOS/AppDelegate.swift
@@ -1,9 +1,8 @@
 //
-//  AppDelegate.swift
-//  Example-iOS
+// Copyright (c) 2016 Ahmad M. Zawawi (azawawi)
 //
-//  Created by Simtix on 10/30/16.
-//  Copyright © 2016 azawawi. All rights reserved.
+// This package is distributed under the terms of the MIT license.
+// Please see the accompanying LICENSE file for the full text of the license.
 //
 
 import UIKit
--- Example-iOS/Example-iOS/ViewController.swift
+++ Example-iOS/Example-iOS/ViewController.swift
@@ -1,18 +1,20 @@
 //
-//  ViewController.swift
-//  Example-iOS
+// Copyright (c) 2016 Ahmad M. Zawawi (azawawi)
 //
-//  Created by Simtix on 10/30/16.
-//  Copyright © 2016 azawawi. All rights reserved.
+// This package is distributed under the terms of the MIT license.
+// Please see the accompanying LICENSE file for the full text of the license.
 //
 
 import UIKit
+import SwiftyZeroMQ
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        print(SwiftyZeroMQ.version.versionString)
+        print(SwiftyZeroMQ.frameworkVersion)
     }
 
     override func didReceiveMemoryWarning() {
