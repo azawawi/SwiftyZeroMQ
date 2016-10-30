42544b7f9f98b75c94dbbce456518d63add70fa2
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-10-30 10:28:00 +0200

    Add initial example project for iOS

---
+++ Example-iOS/Example-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example-iOS
+//
+//  Created by Simtix on 10/30/16.
+//  Copyright © 2016 azawawi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
