9d111c8eb02e89c06082f505453d1fefa1b0a5aa
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-11-02 15:43:12 +0200

    Add example macOS project skeleton

---
+++ Example-macOS/Example-macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Example-macOS
+//
+//  Created by Simtix on 11/2/16.
+//  Copyright © 2016 azawawi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Example-macOS/Example-macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Example-macOS
+//
+//  Created by Simtix on 11/2/16.
+//  Copyright © 2016 azawawi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
