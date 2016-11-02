b788035e5e4a13b23658f2009aeee24c546e20f7
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-11-02 15:07:49 +0200

    Build watchOS as part of testing

--- run-tests.rb
+++ run-tests.rb
@@ -17,20 +17,25 @@ def which(cmd)
 end
 
 # Run test for simulator destination
-def run_tests(scheme, sdk, destination)
+def run_tests(scheme, sdk, destination, action='test')
   puts "Running 'xcodebuild test' for '#{scheme}, #{sdk}, #{destination}'. Please wait..."
-  command = [
-    "xcodebuild",
-    "-project SwiftyZeroMQ.xcodeproj",
-    "-scheme #{scheme}",
-    "-sdk #{sdk}",
-    "-destination '#{destination}'",
-    "-verbose test | xcpretty -c"
-  ].join(" ")
-  ret = system(command)
-  if !ret
-    puts "Failed while executing '#{command}'"
-    exit 1
+
+  # Build or run framework test (if needed) in debug and release configurations
+  for configuration in ["Debug", "Release"]
+    command = [
+      "xcodebuild",
+      "-project SwiftyZeroMQ.xcodeproj",
+      "-scheme #{scheme}",
+      "-sdk #{sdk}",
+      "-destination '#{destination}'",
+      "-configuration #{configuration}",
+      "-verbose #{action} | xcpretty -c"
+    ].join(" ")
+    ret = system(command)
+    if !ret
+      puts "Failed while executing '#{command}'"
+      exit 1
+    end
   end
 end
 
@@ -46,18 +51,24 @@ if which("xcpretty") == nil then
 end
 
 # Run iOS tests
-scheme='SwiftyZeroMQ-iOS'
-sdk='iphonesimulator10.0'
+scheme = 'SwiftyZeroMQ-iOS'
+sdk    = 'iphonesimulator10.0'
 run_tests(scheme, sdk, 'platform=iOS Simulator,name=iPhone 5,OS=9.0')
 run_tests(scheme, sdk, 'platform=iOS Simulator,name=iPhone 5,OS=10.0')
 
 # Run macOS tests
-scheme='SwiftyZeroMQ-macOS'
-sdk="macosx10.12"
+scheme = 'SwiftyZeroMQ-macOS'
+sdk    = 'macosx10.12'
 run_tests(scheme, sdk, 'arch=x86_64')
 
 # Run tvOS tests
-scheme='SwiftyZeroMQ-tvOS'
-sdk="appletvsimulator10.0"
+scheme = 'SwiftyZeroMQ-tvOS'
+sdk    = 'appletvsimulator10.0'
 run_tests(scheme, sdk, 'OS=9.0,name=Apple TV 1080p')
 run_tests(scheme, sdk, 'OS=10.0,name=Apple TV 1080p')
+
+# Only build watchOS since it does not support testing at the moment
+scheme = 'SwiftyZeroMQ-tvOS'
+sdk    = 'watchsimulator3.0'
+run_tests(scheme, sdk, 'OS=2.0,name=Apple Watch - 42mm', 'build')
+run_tests(scheme, sdk, 'OS=3.0,name=Apple Watch - 42mm', 'build')
