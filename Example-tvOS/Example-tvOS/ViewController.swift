95be5444381c3085fc392c4e37ccec5d7d554a78
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-11-02 16:09:54 +0200

    Add example tvOS project

---
+++ Example-tvOS/Example-tvOS/ViewController.swift
@@ -0,0 +1,33 @@
+//
+// Copyright (c) 2016 Ahmad M. Zawawi (azawawi)
+//
+// This package is distributed under the terms of the MIT license.
+// Please see the accompanying LICENSE file for the full text of the license.
+//
+
+import UIKit
+import SwiftyZeroMQ
+
+class ViewController: UIViewController {
+    @IBOutlet weak var versionTextView: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        // Show version information
+        let frameworkVersion = SwiftyZeroMQ.frameworkVersion
+        let version = SwiftyZeroMQ.version.versionString
+        versionTextView.text =
+            "SwiftyZeroMQ version is \(frameworkVersion!)\n" +
+        "ZeroMQ library version is \(version)"
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
