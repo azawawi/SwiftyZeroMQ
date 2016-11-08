1954ded6741e558a951f3eb9753a011c4b33a707
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-11-08 13:03:07 +0200

    Another code review of Poller + tidy

--- Sources/Poller.swift
+++ Sources/Poller.swift
@@ -8,7 +8,7 @@
 extension SwiftyZeroMQ {
 
     /**
-        This represents poll flags and is used in `register` and `modify`
+        This represents the poll flags and is used in `register` and `modify`
      */
     public struct PollFlags : OptionSet {
         public var rawValue: Int32
@@ -17,124 +17,145 @@ extension SwiftyZeroMQ {
             self.rawValue = rawValue
         }
 
-        static let pollIn          = PollFlags(rawValue: ZMQ_POLLIN)
-        static let pollOut         = PollFlags(rawValue: ZMQ_POLLOUT)
-        static let pollErr         = PollFlags(rawValue: ZMQ_POLLERR)
-        static let none: PollFlags = []
+        // Poll flags
+        static let pollIn            = PollFlags(rawValue: ZMQ_POLLIN)
+        static let pollOut           = PollFlags(rawValue: ZMQ_POLLOUT)
+        static let pollErr           = PollFlags(rawValue: ZMQ_POLLERR)
+        static let `none`: PollFlags = []
     }
 
     /**
-        This represents a ZeroMQ poller implementation
+        This represents a ZeroMQ poller implementation which provides socket
+        I/O multiplexing
      */
     public class Poller {
-        private var sockets: [(Socket, PollFlags)]
-        private var socketMap: [Socket:Int]
+        private var sockets   : [(Socket, PollFlags)]
+        private var socketMap : [Socket:Int]
 
         public init() {
-            sockets = [(Socket, PollFlags)]()
+            sockets   = [(Socket, PollFlags)]()
             socketMap = [Socket:Int]()
         }
 
         /**
-          Register a socket for the given flags
-        */
-        public func register(socket: Socket, flags: PollFlags = [.pollIn, .pollOut]) throws {
-            // if there are any flags, update or add the socket as necessary
+            Register a socket for the given flags
+            If no flags are supplied, it will unregister the socket
+         */
+        public func register(
+            socket: Socket,
+            flags: PollFlags = [.pollIn, .pollOut])
+            throws
+        {
             if flags != PollFlags.none {
-                if let ix = self.socketMap[socket] {
-                    self.sockets[ix] = (socket, flags)
+                // if there are any flags, update or add the socket as needed
+                if let socketIndex  = socketMap[socket] {
+                    sockets[socketIndex] = (socket, flags)
                 } else {
-                    let ix = sockets.count
-                    self.sockets.append((socket, flags))
-                    self.socketMap[socket] = ix
+                    let socketIndex        = sockets.count
+                    sockets.append((socket, flags))
+                    socketMap[socket] = socketIndex
                 }
-            }
-            // if no flags but currently registered then unregister
-            else if (self.socketMap[socket] != nil) {
-                try self.unregister(socket: socket)
+            } else if (socketMap[socket] != nil) {
+                // if no flags were supplied but currently registered then
+                // unregister socket
+                try unregister(socket: socket)
             }
 
-            // if socket not currently registered and no flags supplied then do nothing
-
+            // if socket is not currently registered and no flags were supplied
+            // then do nothing
         }
 
         /**
-          Modify a socket registration. This is equivalent to calling
-          `register(socket, flags)`
-        */
-        public func modify(socket: Socket, flags: PollFlags = [.pollIn, .pollOut]) throws {
-            // thin wrapper for register
-            try self.register(socket: socket, flags: flags)
+            Modify a socket registration. This is equivalent to calling
+            `register(socket, flags)`
+         */
+        public func modify(
+            socket : Socket,
+            flags  : PollFlags = [.pollIn, .pollOut]) throws
+        {
+            try register(socket: socket, flags: flags)
         }
 
         /**
-          Unregister the supplied socket
-        */
+            Unregister the supplied socket
+         */
         public func unregister(socket: Socket) throws {
-            let ix = self.socketMap[socket]!
-            self.sockets.remove(at: ix)
+            let socketIndex = socketMap[socket]!
+            sockets.remove(at: socketIndex)
 
-            // update indices of all other sockets in the socket map
-            for (socket, _) in sockets.suffix(from: ix) {
-                let socketIx = self.socketMap[socket]!
-                self.socketMap[socket] = socketIx - 1
+            // Update indices of all other sockets in the socket map
+            for (socket, _) in sockets.suffix(from: socketIndex) {
+                let socketIndex = socketMap[socket]!
+                socketMap[socket] = socketIndex - 1
             }
         }
 
         /**
             Poll the register socket(s) for events
          */
-        public func poll(timeout: TimeInterval? = nil) throws -> [Socket: PollFlags] {
-            // TODO is there a more swifty way of doing this?
-            var intTimeout : Int
-            if timeout == nil {
-                intTimeout = -1
-            } else {
-                intTimeout = Int(timeout!)
-            }
-
-            // now poll
-            let pollItems = Poller.constructPollItems(sockets: self.sockets)
-            let code = zmq_poll(pollItems, Int32(self.sockets.count), intTimeout)
-
-            // if code is negative raise the appropriate error
-            if code < 0 {
-                pollItems.deallocate(capacity: self.sockets.count)
-                throw ZeroMQError.last
-            }
-            // construct return type
-            let ret = Poller.constructReturn(sockets: self.sockets, pollItems: pollItems)
-            pollItems.deallocate(capacity: self.sockets.count)
-            return ret
-        }
-
-        /**
-          Construct return type given sockets
-        */
-        private static func constructReturn(sockets: [(Socket, PollFlags)],
-                                            pollItems: UnsafeMutablePointer<zmq_pollitem_t>)
+        public func poll(timeout: TimeInterval? = nil) throws
             -> [Socket: PollFlags]
         {
-            // assumes that sockets and poll items are in the same order
-            var ret = [Socket: PollFlags]()
-            for (ix, (socket, _)) in sockets.enumerated() {
-                let pollItem      = pollItems[ix]
-                let receivedFlags = PollFlags(rawValue: Int32(pollItem.revents))
-                ret[socket]       = receivedFlags
+            // Now start polling
+            let pollItems = Poller.buildPollItems(sockets: sockets)
+            let intTimeout = (timeout == nil)
+                ? -1
+                : Int(timeout!)
+            let code = zmq_poll(pollItems, Int32(sockets.count),
+              intTimeout)
+
+            if code < 0 {
+                // if code is negative, cleanup poll items before
+                // throwing an error
+                pollItems.deallocate(capacity: sockets.count)
+                throw ZeroMQError.last
             }
-            return ret
+
+            // Build hash map [Socket: PollFlags]
+            let map = buildSocketPollFlagsMap(
+                sockets   : sockets,
+                pollItems : pollItems
+            )
+
+            // Cleanup poll items
+            pollItems.deallocate(capacity: sockets.count)
+
+            return map
         }
 
         /**
-          Construct poll items from a supplied list of socket flags
-        */
-        private static func constructPollItems(sockets: [(Socket, PollFlags)]) -> UnsafeMutablePointer<zmq_pollitem_t> {
-            let pollItems = UnsafeMutablePointer<zmq_pollitem_t>.allocate(capacity: sockets.count)
-            for (ix, (socket, flags)) in sockets.enumerated() {
-                var pollItem    = zmq_pollitem_t()
-                pollItem.socket = socket.handle
-                pollItem.events = Int16(flags.rawValue)
-                pollItems[ix]   = pollItem
+            Build socket to poll flags map from provided sockets and poll items
+         */
+        private func buildSocketPollFlagsMap(
+            sockets   : [(Socket, PollFlags)],
+            pollItems : UnsafeMutablePointer<zmq_pollitem_t>
+        ) -> [Socket: PollFlags]
+        {
+            // Assumption: sockets and poll items are in the same order
+            var map = [Socket: PollFlags]()
+            for (socketIndex, (socket, _)) in sockets.enumerated() {
+                let pollItem      = pollItems[socketIndex]
+                let receivedFlags = PollFlags(rawValue: Int32(pollItem.revents))
+                map[socket]       = receivedFlags
+            }
+            return map
+        }
+
+        /**
+            Build poll items from a supplied list of socket flags
+         */
+        private static func buildPollItems(
+            sockets: [(Socket, PollFlags)]
+        ) -> UnsafeMutablePointer<zmq_pollitem_t>
+        {
+            let pollItems = UnsafeMutablePointer<zmq_pollitem_t>.allocate(
+              capacity: sockets.count
+            )
+            for (socketIndex, (socket, flags)) in sockets.enumerated() {
+                var pollItem           = zmq_pollitem_t()
+                pollItem.socket        = socket.handle
+                pollItem.events        = Int16(flags.rawValue)
+                pollItems[socketIndex] = pollItem
             }
             return pollItems
         }
