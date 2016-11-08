2f855c927aaab8b2afeac5d4f5acae59f50b51d1
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-11-08 12:32:59 +0200

    Less boilerplate in tests

--- Tests/PollerTests.swift
+++ Tests/PollerTests.swift
@@ -11,16 +11,6 @@ import XCTest
 class PollerTests: XCTestCase {
     private let endpoint = "tcp://127.0.0.1:5555"
 
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
     func testRequestReply() throws {
         let context    = try SwiftyZeroMQ.Context()
         let replier    = try context.socket(.reply)
--- Tests/ZeroMQTests.swift
+++ Tests/ZeroMQTests.swift
@@ -10,20 +10,6 @@ import XCTest
 
 class ZeroMQTests: XCTestCase {
 
-    override func setUp() {
-        super.setUp()
-
-        // Put setup code here. This method is called before the invocation of
-        // each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation
-        // of each test method in the class.
-
-        super.tearDown()
-    }
-
     func testVersion() {
         let (major, minor, patch, versionString) = SwiftyZeroMQ.version
         XCTAssertTrue(major == 4, "Major version is 4")
