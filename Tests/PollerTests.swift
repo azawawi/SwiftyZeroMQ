6c058131aaa241c3e864e6861cd86bbfe1eb389d
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-11-14 10:33:58 +0200

    More source code documentation

--- Tests/PollerTests.swift
+++ Tests/PollerTests.swift
@@ -12,52 +12,60 @@ class PollerTests: XCTestCase {
     private let endpoint = "tcp://127.0.0.1:5555"
 
     func testRequestReply() throws {
+        // Context
         let context    = try SwiftyZeroMQ.Context()
+
+        // Replier socket
         let replier    = try context.socket(.reply)
         try replier.bind(endpoint)
+
+        // Requester socket
         let requestor  = try context.socket(.request)
         try requestor.connect(endpoint)
 
+        // Register requester and replier sockets in a new poller object
         let poller = SwiftyZeroMQ.Poller()
-
         try poller.register(socket: replier,   flags: [.pollIn, .pollOut])
         try poller.register(socket: requestor, flags: [.pollIn, .pollOut])
 
-        // Initial states: replier should be in none, requestor in pollIn
+        // Replier should initially be in .none and requestor in .pollIn
         var socks = try poller.poll()
         XCTAssertEqual(socks[replier],   SwiftyZeroMQ.PollFlags.none)
         XCTAssertEqual(socks[requestor], SwiftyZeroMQ.PollFlags.pollOut)
 
-        // Requestor should go into none right after sending request
+        // Requestor should go into .none right after sending request
         try requestor.send(string: "msg1", options: .dontWait)
         socks = try poller.poll()
         XCTAssertEqual(socks[requestor], SwiftyZeroMQ.PollFlags.none)
 
-        // After a short wait replier should go into pollIn
+        // After a short wait replier should go into .pollIn
         sleep(1)
         socks = try poller.poll()
 
         XCTAssertEqual(socks[replier], SwiftyZeroMQ.PollFlags.pollIn)
 
-        // After receive, replier should go into pollOut
+        // After receive, replier should go into .pollOut
         try _ = replier.recv()
         socks = try poller.poll()
         XCTAssertEqual(socks[replier], SwiftyZeroMQ.PollFlags.pollOut)
     }
 
     func testPair() throws {
+        // Context
         let context = try SwiftyZeroMQ.Context()
+
+        // Create two pair sockets (bind, connect)
         let s1      = try context.socket(.pair)
         try s1.bind(endpoint)
         let s2      = try context.socket(.pair)
         try s2.connect(endpoint)
 
-        let poller = SwiftyZeroMQ.Poller()
-
+        // Register pair sockets in a new poller object
+        let poller  = SwiftyZeroMQ.Poller()
         try poller.register(socket: s1, flags: [.pollIn, .pollOut])
         try poller.register(socket: s2, flags: [.pollIn, .pollOut])
 
-        // Initial states: both should be in pollOut
+        // Initially both should be in .pollOut
         var socks = try poller.poll()
         XCTAssertEqual(socks[s1], SwiftyZeroMQ.PollFlags.pollOut)
         XCTAssertEqual(socks[s2], SwiftyZeroMQ.PollFlags.pollOut)
@@ -66,12 +74,13 @@ class PollerTests: XCTestCase {
         try s1.send(string: "msg1")
         try s2.send(string: "msg2")
 
+        // After a wait, both should be [.pollout, .pollIn]
         sleep(1)
         socks = try poller.poll()
         XCTAssertEqual(socks[s1], [SwiftyZeroMQ.PollFlags.pollOut, SwiftyZeroMQ.PollFlags.pollIn])
         XCTAssertEqual(socks[s2], [SwiftyZeroMQ.PollFlags.pollOut, SwiftyZeroMQ.PollFlags.pollIn])
 
-        // Now recv both and test they are both back in pollIn
+        // Now read from both and test they are both back in .pollOut
         try _ = s1.recv()
         try _ = s2.recv()
         socks = try poller.poll()
