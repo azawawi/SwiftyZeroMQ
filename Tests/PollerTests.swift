fba3d99205fecdf74fa3cca43f3dd01c93b32563
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-11-14 10:26:27 +0200

    Capitialize comments in poller tests

--- Tests/PollerTests.swift
+++ Tests/PollerTests.swift
@@ -23,23 +23,23 @@ class PollerTests: XCTestCase {
         try poller.register(socket: replier,   flags: [.pollIn, .pollOut])
         try poller.register(socket: requestor, flags: [.pollIn, .pollOut])
 
-        // initial states: replier should be in none, requestor in pollIn
+        // Initial states: replier should be in none, requestor in pollIn
         var socks = try poller.poll()
         XCTAssertEqual(socks[replier],   SwiftyZeroMQ.PollFlags.none)
         XCTAssertEqual(socks[requestor], SwiftyZeroMQ.PollFlags.pollOut)
 
-        // requestor should go into none right after sending request
+        // Requestor should go into none right after sending request
         try requestor.send(string: "msg1", options: .dontWait)
         socks = try poller.poll()
         XCTAssertEqual(socks[requestor], SwiftyZeroMQ.PollFlags.none)
 
-        // after a short wait replier should go into pollIn
+        // After a short wait replier should go into pollIn
         sleep(1)
         socks = try poller.poll()
 
         XCTAssertEqual(socks[replier], SwiftyZeroMQ.PollFlags.pollIn)
 
-        // after receive, replier should go into pollOut
+        // After receive, replier should go into pollOut
         try _ = replier.recv()
         socks = try poller.poll()
         XCTAssertEqual(socks[replier], SwiftyZeroMQ.PollFlags.pollOut)
@@ -57,12 +57,12 @@ class PollerTests: XCTestCase {
         try poller.register(socket: s1, flags: [.pollIn, .pollOut])
         try poller.register(socket: s2, flags: [.pollIn, .pollOut])
 
-        // initial states: both should be in pollOut
+        // Initial states: both should be in pollOut
         var socks = try poller.poll()
         XCTAssertEqual(socks[s1], SwiftyZeroMQ.PollFlags.pollOut)
         XCTAssertEqual(socks[s2], SwiftyZeroMQ.PollFlags.pollOut)
 
-        // send on both, then both should enter pollIn
+        // Send on both, then both should enter pollIn
         try s1.send(string: "msg1")
         try s2.send(string: "msg2")
 
@@ -71,7 +71,7 @@ class PollerTests: XCTestCase {
         XCTAssertEqual(socks[s1], [SwiftyZeroMQ.PollFlags.pollOut, SwiftyZeroMQ.PollFlags.pollIn])
         XCTAssertEqual(socks[s2], [SwiftyZeroMQ.PollFlags.pollOut, SwiftyZeroMQ.PollFlags.pollIn])
 
-        // now recv both and test they are both back in pollIn
+        // Now recv both and test they are both back in pollIn
         try _ = s1.recv()
         try _ = s2.recv()
         socks = try poller.poll()
