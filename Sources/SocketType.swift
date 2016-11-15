4af943cf54dd60983f908cffa4f36da868059294
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-11-15 11:05:13 +0200

    More doc updates

--- Sources/SocketType.swift
+++ Sources/SocketType.swift
@@ -11,36 +11,41 @@ extension SwiftyZeroMQ {
         An enumeration of ZeroMQ socket types that maps to a signed 32-bit
         integer. This provides the following patterns:
 
-        - Request-reply pattern: `.request`, `.reply`, `.router`, `.dealer`
-          The request-reply pattern is used for sending requests from a `.request`
-          client to one or more `.reply` services, and receiving subsequent
-          replies to each request sent. The request-reply pattern is formally
-          defined [here](http://rfc.zeromq.org/spec:28).
+        - Request-reply pattern (`.request`, `.reply`, `.router`, `.dealer`):
 
-        - Publish-subscribe pattern: `.publish`, `.subscribe`, `.xpublish`,
-          `.xsubscribe`
-          The publish-subscribe pattern is used for one-to-many distribution of
-          data from a single publisher to multiple subscribers in a fan out
-          fashion. The publish-subscribe pattern is formally defined
-          [here](http://rfc.zeromq.org/spec:29)
+            The request-reply pattern is used for sending requests from a
+            `.request` client to one or more `.reply` services, and receiving
+            subsequent replies to each request sent. The request-reply pattern
+            is formally defined [here](http://rfc.zeromq.org/spec:28).
 
-        - Pipeline pattern: `.push`, `.pull`
-          The pipeline pattern is used for distributing data to nodes arranged
-          in a pipeline. Data always flows down the pipeline, and each stage of
-          the pipeline is connected to at least one node. When a pipeline stage
-          is connected to multiple nodes data is round-robined among all
-          connected nodes. The pipeline pattern is formally defined
-          [here](http://rfc.zeromq.org/spec:30).
+        - Publish-subscribe pattern (`.publish`, `.subscribe`, `.xpublish`,
+          `.xsubscribe`):
 
-        - Exclusive pair pattern: `.pair`
-          The exclusive pair pattern is used to connect a peer to precisely one
-          other peer. This pattern is used for inter-thread communication across
-          the inproc transport. The exclusive pair pattern is formally defined
-          [here](http://rfc.zeromq.org/spec:31).
+            The publish-subscribe pattern is used for one-to-many distribution
+            of data from a single publisher to multiple subscribers in a fan out
+            fashion. The publish-subscribe pattern is formally defined
+            [here](http://rfc.zeromq.org/spec:29).
 
-        - Native pattern: `.stream`
-          The native pattern is used for communicating with TCP peers and allows
-          asynchronous requests and replies in either direction.
+        - Pipeline pattern (`.push`, `.pull`):
+
+            The pipeline pattern is used for distributing data to nodes arranged
+            in a pipeline. Data always flows down the pipeline, and each stage
+            of the pipeline is connected to at least one node. When a pipeline
+            stage is connected to multiple nodes data is round-robined among all
+            connected nodes. The pipeline pattern is formally defined
+            [here](http://rfc.zeromq.org/spec:30).
+
+        - Exclusive pair pattern (`.pair`):
+
+            The exclusive pair pattern is used to connect a peer to precisely
+            one other peer. This pattern is used for inter-thread communication
+            across the inproc transport. The exclusive pair pattern is formally
+            defined [here](http://rfc.zeromq.org/spec:31).
+
+        - Native pattern (`.stream`):
+
+            The native pattern is used for communicating with TCP peers and
+            allows asynchronous requests and replies in either direction.
      */
     public enum SocketType : Int32 {
         // Request-reply pattern
